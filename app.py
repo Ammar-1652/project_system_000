60ea4337867946777d98110e36c86049d4ffd3e0
A7medGaber <www.a7medgaber12@gmail.com>
2023-12-14 10:45:15 +0200

    zzz

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ def courses_for_admin():
         day=request.form.get('day'))
         db.session.add(c)
         db.commit()
-    return render_template("courses_for_admin.html",course=course , course_show=course_show)
+    return render_template("courses_for_admin.html",c=c , course_show=course_show)
 
 
 @app.route("/timetable_for_admin")
